9cc5ce90bc41e49d82c95eb6245926290b46aeb5
Simone Baratta <Conte91@gmail.com>
2015-09-15 21:25:20 +0200

    Fix and optimize CPLD architecture - RTL wins, behavioural loses

--- MouseWheel/MouseWheelHandler.vhd
+++ MouseWheel/MouseWheelHandler.vhd
@@ -16,29 +16,16 @@ UNDERFLOW: OUT std_logic
 END MouseWheelHandler;
 
 ARCHITECTURE Structural OF MouseWheelHandler IS
---signal toBeAdded: unsigned(6 downto 0);
---signal newValue: unsigned(6 downto 0);
---signal level_bits: std_logic_vector(6 downto 0);
---signal theSum: unsigned(6 downto 0);
---signal theSum_bits: std_logic_vector(6 downto 0);
+signal theSum: unsigned(7 downto 0);
+signal whatToSum: unsigned(7 downto 0);
 signal myLevel: unsigned(7 downto 0);
+signal newValue: unsigned(7 downto 0);
 signal btnDown: std_logic;
 signal BTN1_r, BTN2_r, BTN3_r, BTN1_p, BTN2_p: std_logic;
 signal have_done_overflow,have_done_underflow: std_logic;
+signal bypass: std_logic;
 BEGIN
---NBTN1 <= not BTN1_r;
---NBTN2 <= not BTN2_r;
---NBTN3 <= not BTN3_r;
---NBTNCLK <= btnDown_n;
---NRST <= not theRST;
-
---BTN1_p<=BTN1;
---BTN2_p<=BTN2;
---BTN1_r<=BTN1;
---BTN2_r<=BTN2;
---BTN3_r<=BTN3;
---BTNDown<=BTN1 or BTN2;
-regRobe: PROCESS(BTNCLK) BEGIN
+twoFlipFlops: PROCESS(BTNCLK) BEGIN
 	IF(BTNCLK'event and BTNCLK='1') THEN
 	BTN1_p<=BTN1_r;
 	BTN2_p<=BTN2_r;
@@ -49,68 +36,38 @@ regRobe: PROCESS(BTNCLK) BEGIN
 	END IF;
 END PROCESS;
 
-doTheCalculation: PROCESS(BTNDown, RST, BTN3_r)
-  VARIABLE level: unsigned(7 downto 0);
-  VARIABLE maybe: unsigned(7 downto 0);
-BEGIN 
-  IF (RST='1' or BTN3_r='1') THEN
-		level:="01111111";
-		mylevel<=level;
-  ELSIF(BTNDown'event AND BTNDown='0') THEN
-		IF (BTN1_p='1' AND ((level(7) and level(6) and level(5) and level(4) and level(3) and level(2) and level(1) and level(0)))='0') THEN
-			level := level+"00000001";
-		mylevel<=level;
-		ELSIF(BTN2_p='1' AND (((not level(7)) and (not level(6)) and (not level(5))))='0') THEN
-			level := level-"00000001";
-		mylevel<=level;
-		END IF;
+whatToSumMux: PROCESS(BTN1_p) BEGIN
+	IF(BTN1_p='1') THEN
+		whatToSum <= (0 => '1', OTHERS => '0');
+	ELSE
+		whatToSum <= (OTHERS => '1');
 	END IF;
 END PROCESS;
-		
-	
---regRobe: PROCESS(BTNCLK) BEGIN
---	IF(BTNCLK'event and BTNCLK='1') THEN
---		BTN1_r<=BTN1;
---		BTN2_r<=BTN2;
---		BTN3_r<=BTN3;
---	END IF;
---END PROCESS;
---	
+
+theSum <= mylevel + whatToSum;
+
+bypass <= ((BTN1_p and have_done_overflow) OR (BTN2_p and have_done_underflow));
+
+bypassSumMux: PROCESS(bypass, mylevel, thesum) BEGIN
+	IF(bypass='1') THEN
+		newValue <= mylevel;
+	ELSE
+		newValue <= theSum;
+	END IF;
+END PROCESS;
+
+doTheCalculation: PROCESS(BTNDown, RST, BTN3_r)
+BEGIN 
+  IF (RST='1' or BTN3_r='1') THEN
+		mylevel<="01111111";
+  ELSIF(BTNDown'event AND BTNDown='0') THEN
+		mylevel<=newValue;
+	END IF;
+END PROCESS;
+
 THELEVEL <= std_logic_vector(myLevel);
---btnDown <= BTN1_r or BTN2_r;
 have_done_overflow<=  (mylevel(7) and mylevel(6) and mylevel(5) and mylevel(4) and mylevel(3) and mylevel(2) and mylevel(1) and mylevel(0));
 have_done_underflow <= (not mylevel(7)) and (not mylevel(6)) and (not mylevel(5));
---bypass <= (have_done_overflow and summingUp) or (have_done_underflow and (not summingUp));
---theRST <= RST or BTN3_r;
---
---toBeAdded<="0000001";
---whatToSum: PROCESS(BTNDown) BEGIN
---	IF(BTNDown'event AND BTNDown='1') THEN
---		IF(BTN1_r='1') THEN
---			newValue<=level + toBeAdded;
---			summingUp<= '1';
---		ELSE
---			newValue<= level-toBeAdded;
---			summingUp<= '0';
---		END IF;
---	END IF;
---END PROCESS;
---
---rst_n <= not theRST;
---btnDown_n <= not btnDown;
-----currentValue: PROCESS(theRST, BTNCLK)
---currentValue: ENTITY work.Reg GENERIC MAP(7, 63)
---PORT MAP(input => theSum_bits,
---     output=> level_bits,
---    		 rst => rst_n,
---       writeCLK => btnDown_n
---		 );
---level <= unsigned(level_bits);
---theSum <= level when bypass='1' else newValue;
---theSum_bits <= std_logic_vector(theSum);
 OVERFLOW <= have_done_overflow;
 UNDERFLOW <= have_done_underflow;
---THELEVEL <= level_bits;		 
---BYPASS_P <= not bypass;
---SUM <= theSum_bits;
 END ARCHITECTURE;
--- MouseWheel/MouseWheelHandlerTB.vhd
+++ MouseWheel/MouseWheelHandlerTB.vhd
@@ -4,7 +4,7 @@ ENTITY MouseHandlerTB IS
 END MouseHandlerTB;
 
 ARCHITECTURE TB OF MouseHandlerTB IS
-signal THELEVEL:  std_logic_vector(6 downto 0);
+signal THELEVEL:  std_logic_vector(7 downto 0);
 signal BTN1:  std_logic;
 signal BTN2:  std_logic;
 signal BTN3:  std_logic;
@@ -17,39 +17,45 @@ BEGIN
 uut: ENTITY work.MouseWheelHandler 
 PORT MAP(THELEVEL, BTN1, BTN2, BTN3, RST, CLK, OVERFLOW, UNDERFLOW);
 
+clockit: PROCESS BEGIN
+  FOR i in 0 to 20000 loop
+    wait for 0.22ms;
+    CLK <= not CLK;
+  END LOOP;
+  wait;
+END PROCESS;
 
-CLK <= not CLK after 1.25ms;
 test: PROCESS BEGIN
 	BTN1<='0';
 	BTN2<='0';
 	BTN3<='0';
 	RST <= '0';
-	wait for 1ms;
+	wait for 2ms;
 	RST <= '1';
-	wait for 1ms;
+	wait for 2ms;
 	RST <= '0';
-	wait for 1ms;
+	wait for 2ms;
 	for i in 0 to 10 loop
 		BTN1<='1';
-		wait for 1ms;
+		wait for 2ms;
 		BTN1<='0';
-		wait for 1ms;
+		wait for 2ms;
 	end loop;
 	BTN3 <= '1';
-	wait for 1ms;
+	wait for 2ms;
 	BTN3 <= '0';
-	wait for 1ms;
+	wait for 2ms;
 	for i in 0 to 200 loop
 		BTN1<='1';
-		wait for 1ms;
+		wait for 2ms;
 		BTN1<='0';
-		wait for 1ms;
+		wait for 2ms;
 	end loop;
 		for i in 0 to 500 loop
 		BTN2<='1';
-		wait for 1ms;
+		wait for 2ms;
 		BTN2<='0';
-		wait for 1ms;
+		wait for 2ms;
 	end loop;
 	wait;
 END PROCESS;
